d75ee201e93ab13dc1902b7c57702d13b7456f1c
Jon <jtsang109@gmail.com>
2015-11-02 22:31:41 -0500

    new py

---
+++ randocondiment.py
@@ -0,0 +1,18 @@
+import random
+random.seed()
+x = random.randint(1, 4)
+
+if x == 1:
+    print "Ketchup and Mustard Only"
+
+if x == 2:
+    print "Pickles, lots and lots of pickles"
+
+if x == 3:
+    print "Peanut Butter and Jelly"
+
+if x == 4:
+    print "Pepperoni, Spinach, BBQ Sauce, and Mayonaise"
+
+if x == 5:
+    print "lots of salt and maple syrup for your popcorn pancakes"
